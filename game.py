ed14464815e55d531689e83733e406738d615b42
motasem oran <muatasemoran@gmail.com>
2021-04-11 11:47:34 +0300

    added map loop method, will automatically draw the map now

--- game.py
+++ game.py
@@ -2,6 +2,13 @@ import pygame
 import maps
 from entities import grass, rock, hole, player, box, empty
 
+black = (0, 0, 0)
+which = (255, 255, 255)
+gray = (192, 192, 192)
+green = (0, 128, 0)
+brown = (150, 75, 0)
+player = (255, 255, 255)
+
 def make_grid(surf):
     size_block = width / rows
     x, y = 0, 0
@@ -11,15 +18,17 @@ def make_grid(surf):
         pygame.draw.line(surf, (255, 255, 255), (x, 0), (x, width))
         pygame.draw.line(surf, (255, 255, 255),(0, y), (width, y))
 
-def re_make_window(surf):
+def re_make_window(surf, level):
     surf.fill((255, 255, 255))
     make_grid(surf)
+    for pos, ent in level.items():
+        ent.draw(surf, pos, block_size)
     pygame.display.update()
 
 def game():
     global width, rows, block_size
     width = 800
-    rows = 100
+    rows = 8
     block_size = width / rows
     window = pygame.display.set_mode((width, width))
     clock = pygame.time.Clock()
@@ -29,6 +38,6 @@ def game():
         pygame.event.pump()
         pygame.time.delay(5)
         clock.tick(10)
-        re_make_window(window)
+        re_make_window(window, level)
 
 game()
