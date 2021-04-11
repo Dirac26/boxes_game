064992bd5be0dad9e5e25bbc7b21e9b924307bd6
motasem oran <muatasemoran@gmail.com>
2021-04-11 09:50:26 +0300

    added level 1 getter

---
+++ maps.py
@@ -0,0 +1,37 @@
+from entities import grass, rock, hole, player, box, empty
+
+r = rock()
+p = player()
+b1 = box()
+b2 = box()
+b3 = box()
+
+e = empty()
+
+h1 = hole(e)
+h2 = hole(e)
+h3 = hole(e)
+
+
+
+level_1 = [
+           [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)], 
+           [grass(r), grass(e), grass(b1), h1, grass(r), grass(r), grass(r), grass(r)],
+           [grass(r), grass(e), grass(e), h2, h3, grass(p), grass(r), grass(r)],
+           [grass(r), grass(e), grass(e), grass(r), grass(r), grass(b3), grass(r), grass(r)],
+           [grass(r), grass(r), grass(e), grass(e), grass(r), grass(e), grass(e), grass(r)],
+           [grass(r), grass(r), grass(b2), grass(e), grass(e), grass(e), grass(e), grass(r)],
+           [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+           [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)],
+
+]
+
+def get_level_1():
+    level_1_dict = {}
+    for i, row in enumerate(level_1):
+        for j, val in enumerate(row):
+            level_1_dict[(i, j)] = val
+    
+    return level_1_dict, p, b1, b2, b3, h1, h2, h3
+
+
