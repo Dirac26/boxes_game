b141d3fb2d0fbc412972e521e70bc0a6b0e28a17
motasem oran <muatasemoran@gmail.com>
2021-04-14 19:57:46 +0300

    changed maps to classes each class is a stratigy for new map

--- maps.py
+++ maps.py
@@ -1,5 +1,5 @@
 from entities import grass, rock, hole, player, box, empty
-
+from abc import ABC, abstractmethod
 r = rock()
 p = player()
 b1 = box()
@@ -12,25 +12,42 @@ h1 = hole(e)
 h2 = hole(e)
 h3 = hole(e)
 
+class Map(ABC):    
+    def __iter__(self):
+        self.i = 0
+        self.j = 0
+        return self
+    @abstractmethod
+    def get_player(self):
+        pass
+    @abstractmethod
+    def get_name(self):
+        pass
+    @abstractmethod
+    def __len__(self):
+        pass
 
+class Map1(Map):
+    def __init__(self):
+        self.level = [
+            [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)], 
+            [grass(r), grass(p), grass(b1), h1, grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(e), grass(b3), h2, h3, grass(e), grass(r), grass(r)],
+            [grass(r), grass(e), grass(e), grass(r), grass(r), grass(e), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(e), grass(e), grass(r)],
+            [grass(r), grass(r), grass(b2), grass(e), grass(e), grass(e), grass(e), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)],
+    ]
 
-level_1 = [
-           [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)], 
-           [grass(r), grass(p), grass(b1), h1, grass(r), grass(r), grass(r), grass(r)],
-           [grass(r), grass(e), grass(b3), h2, h3, grass(e), grass(r), grass(r)],
-           [grass(r), grass(e), grass(e), grass(r), grass(r), grass(e), grass(r), grass(r)],
-           [grass(r), grass(r), grass(e), grass(e), grass(r), grass(e), grass(e), grass(r)],
-           [grass(r), grass(r), grass(b2), grass(e), grass(e), grass(e), grass(e), grass(r)],
-           [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
-           [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)],
+    def get_player(self):
+        return p
+    
+    def get_name(self):
+        return "level 1"
 
-]
+    def __len__(self):
+        return len(self.level[0])
 
-def get_level_1():
-    level_1_dict = {}
-    for i, row in enumerate(level_1):
-        for j, val in enumerate(row):
-            level_1_dict[(j, i)] = val
-    return level_1_dict, p, h1, h2, h3
 
 
