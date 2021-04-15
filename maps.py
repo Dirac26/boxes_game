b344e6bab647d1dca3a4fcbf5fe1a8379ff8aaa3
motasem oran <muatasemoran@gmail.com>
2021-04-15 19:05:06 +0300

    added level getter method

--- maps.py
+++ maps.py
@@ -2,52 +2,71 @@ from entities import grass, rock, hole, player, box, empty
 from abc import ABC, abstractmethod
 r = rock()
 p = player()
-b1 = box()
-b2 = box()
-b3 = box()
-
 e = empty()
-
-h1 = hole(e)
-h2 = hole(e)
-h3 = hole(e)
 
 class Map(ABC):    
     def __iter__(self):
         self.i = 0
         self.j = 0
-        return self
-    @abstractmethod
-    def get_player(self):
-        pass
+        yield self
+
     @abstractmethod
     def get_name(self):
         pass
+
     @abstractmethod
     def __len__(self):
         pass
+
+    def get_level(self):
+        return self.level
+
+    def get_player(self):
+        return p
 
 class Map1(Map):
     def __init__(self):
         self.level = [
             [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)], 
-            [grass(r), grass(p), grass(b1), h1, grass(r), grass(r), grass(r), grass(r)],
-            [grass(r), grass(e), grass(b3), h2, h3, grass(e), grass(r), grass(r)],
+            [grass(r), grass(p), grass(box()), hole(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(e), grass(box()), hole(e), hole(e), grass(e), grass(r), grass(r)],
             [grass(r), grass(e), grass(e), grass(r), grass(r), grass(e), grass(r), grass(r)],
             [grass(r), grass(r), grass(e), grass(e), grass(r), grass(e), grass(e), grass(r)],
-            [grass(r), grass(r), grass(b2), grass(e), grass(e), grass(e), grass(e), grass(r)],
+            [grass(r), grass(r), grass(box()), grass(e), grass(e), grass(e), grass(e), grass(r)],
             [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
             [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r)],
     ]
-
-    def get_player(self):
-        return p
-    
     def get_name(self):
         return "level 1"
 
     def __len__(self):
         return len(self.level[0])
+
+class Map2(Map):
+    def __init__(self):
+        self.level = [
+            [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), hole(e), grass(r), grass(r), grass(r), grass(r)], 
+            [grass(r), grass(p), grass(box()), hole(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), hole(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(box()), hole(e), hole(e), grass(e), grass(r), grass(r), grass(r), grass(e), grass(e), hole(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(e), grass(e), grass(r), grass(r), grass(e), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(e), grass(e), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(box()), grass(e), grass(e), grass(e), grass(e), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)],
+            [grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(r), grass(e), grass(e), grass(e), grass(r), grass(r), grass(r), grass(r)]
+    ]
+    def get_name(self):
+        return "level 2"
+
+    def __len__(self):
+        return len(self.level[0])
+
 
 
 
